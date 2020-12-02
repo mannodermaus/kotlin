f61981a6991c1df23cffb342fa81b922e8ddac24
Dmitriy Novozhilov <dmitriy.novozhilov@jetbrains.com>
2020-12-02 17:22:12 +0300

    [TEST] Add implementation of new infrastructure services for compiler tests
    All of new classes lays in lays in :compiler:tests-common-new module
      which includes classes for FE 1.0 and FIR diagnostics tests and
      JVM black boxtests

---
+++ compiler/tests-common-new/tests/org/jetbrains/kotlin/test/frontend/classic/handlers/FirTestDataConsistencyHandler.kt
@@ -0,0 +1,34 @@
+/*
+ * Copyright 2010-2020 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
+ */
+
+package org.jetbrains.kotlin.test.frontend.classic.handlers
+
+import org.jetbrains.kotlin.codeMetaInfo.clearTextFromDiagnosticMarkup
+import org.jetbrains.kotlin.test.directives.FirDiagnosticsDirectives
+import org.jetbrains.kotlin.test.directives.model.DirectivesContainer
+import org.jetbrains.kotlin.test.model.AfterAnalysisChecker
+import org.jetbrains.kotlin.test.services.TestServices
+import org.jetbrains.kotlin.test.services.assertions
+import org.jetbrains.kotlin.test.services.moduleStructure
+import org.jetbrains.kotlin.test.utils.firTestDataFile
+
+class FirTestDataConsistencyHandler(testServices: TestServices) : AfterAnalysisChecker(testServices) {
+    override val directives: List<DirectivesContainer>
+        get() = listOf(FirDiagnosticsDirectives)
+
+    override fun check(failedAssertions: List<AssertionError>) {
+        val moduleStructure = testServices.moduleStructure
+        val testData = moduleStructure.originalTestDataFiles.first()
+        if (testData.extension == "kts") return
+        if (FirDiagnosticsDirectives.FIR_IDENTICAL in moduleStructure.allDirectives) return
+        val firTestData = testData.firTestDataFile
+        val originalFileContent = clearTextFromDiagnosticMarkup(testData.readText())
+        val firFileContent = clearTextFromDiagnosticMarkup(firTestData.readText())
+        testServices.assertions.assertEquals(originalFileContent, firFileContent) {
+            "Original and fir test data aren't identical. " +
+                    "Please, add changes from ${testData.name} to ${firTestData.name}"
+        }
+    }
+}
